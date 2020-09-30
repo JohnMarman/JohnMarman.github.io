916bcc1738268db9bd4ddb71ddfea7f52df9ce8d
JohnMarman <17261899+JohnMarman@users.noreply.github.com>
2020-09-30 14:18:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ if (canvas.getContext)
 	var context = canvas.getContext('2d');
 	canvas.width  = window.innerWidth;
 	canvas.height = window.innerHeight;
-	context.beginPath(); 
+	
+}
+
+function drawLine() {
+  context.beginPath(); 
 	context.moveTo(canvas.width/2,canvas.height/2);
 	context.lineTo((canvas.width/2)-5,canvas.height/2);
 	context.lineTo((canvas.width/2)-5,(canvas.height/2)-20);
@@ -13,4 +17,14 @@ if (canvas.getContext)
 	context.lineWidth = 1;
 	context.strokeStyle = 'white';
 	context.stroke();
+}
+
+function animate(ratio) {
+  ratio = ratio || 0;
+  drawLine(0,0,300,300,ratio);
+  if(ratio<1) {
+    requestAnimationFrame(function() {
+      animate(ratio + 0.01);
+    });
+  }
 }
