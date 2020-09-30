020c3aeb778843b5f77115da008e5f8b3208f343
JohnMarman <17261899+JohnMarman@users.noreply.github.com>
2020-09-30 13:11:59 -0400

    E

---
+++ script.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById('ScreenCanvas');
+if (canvas.getContext)
+{
+	var context = canvas.getContext('2d');
+	canvas.width  = window.innerWidth;
+	canvas.height = window.innerHeight;
+	context.beginPath(); 
+	context.moveTo(canvas.width/2,canvas.height/2);
+	context.lineTo((canvas.width/2)-5,canvas.height/2);
+	context.lineTo((canvas.width/2)-5,(canvas.height/2)-20);
+	context.lineTo((canvas.width/2),(canvas.height/2)-20);
+	context.lineJoin = 'round';
+	context.lineWidth = 1;
+	context.strokeStyle = 'white';
+	context.stroke();
+}
