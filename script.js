0f75a8f1b938bf6654d0d3ca30771841fc580bd9
JohnMarman <17261899+JohnMarman@users.noreply.github.com>
2020-10-01 12:18:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,24 @@ if (canvas.getContext)
 	canvas.height = window.innerHeight;
 }
 
-function defineLeftBracket() {
-	context.beginPath(); 
-	context.moveTo(canvas.width/2,canvas.height/8);
-	context.lineTo((canvas.width/2)-10,canvas.height/8);
-	context.lineTo((canvas.width/2)-10,(canvas.height/8)-50);
-	context.lineTo((canvas.width/2),(canvas.height/8)-50);
+function drawLeftBracket(offset) {
+	context.beginPath();
+	startpointW = canvas.width/2 - offset;
+	startpointH = canvas.height/8;
+	context.moveTo(startpointW,startpointH);
+	context.lineTo((startpointW-10) - offsetX,startpointH);
+	context.lineTo(startpointW-10,startpointH-50);
+	context.lineTo(startpointW,startpointH-50);
 	context.lineJoin = 'round';
 	context.lineWidth = 0.5;
 	context.strokeStyle = 'black';
 	context.stroke();
 }
 
-function defineRightBracket(){
-	context.beginPath(); 
+function drawRightBracket(){
+	context.beginPath();
+	startpointW = canvas.width/2 + offset;
+	startpointH = canvas.height/8;
 	context.moveTo(canvas.width/2,canvas.height/8);
 	context.lineTo((canvas.width/2)+10,canvas.height/8);
 	context.lineTo((canvas.width/2)+10,(canvas.height/8)-50);
@@ -30,52 +34,50 @@ function defineRightBracket(){
 	context.stroke();
 }
 
-function drawLeftLine(current){
+function drawLeftLine(offset){
 	context.beginPath(); 
 	context.moveTo(canvas.width/2,canvas.height/8);
-	if (current > 0)
+	if (offset > 0)
 	{
-		context.lineTo(canvas.width/2 - current,canvas.height/8);
+		context.lineTo(canvas.width/2 - offset,canvas.height/8);
 	}
 	context.lineWidth = 0.5;
 	context.strokeStyle = 'black';
 	context.stroke();
 }
 
-function drawRightLine(current){
+function drawRightLine(offset){
 	context.beginPath(); 
 	context.moveTo(canvas.width/2,canvas.height/8);
-	if (current < canvas.width)
+	if (offset < canvas.width)
 	{
-		context.lineTo(canvas.width/2 + current,canvas.height/8);
+		context.lineTo(canvas.width/2 + offset,canvas.height/8);
 	}
 	context.lineWidth = 0.5;
 	context.strokeStyle = 'black';
 	context.stroke();
 }
 
-function drawBottomLine(current){
+function drawBottomLine(offset){
 	context.beginPath(); 
 	context.moveTo(canvas.width/2,0);
-	if (current < canvas.height) 
+	if (offset < canvas.height) 
 	{
-		context.lineTo(canvas.width/2,current);
+		context.lineTo(canvas.width/2,offset);
 	}
 	context.lineWidth = 0.5;
 	context.strokeStyle = 'black';
 	context.stroke();
 }
 
-function animate(current) {
-	current = current || 0;
-	drawLeftLine(current);
-	drawRightLine(current);
-	drawBottomLine(current);
-	requestAnimationFrame(function() {
-		animate(current + 1);
-	});
+function animate(offset) {
+	offset = offset || 0;
+	drawLeftBracket();
+	drawRightBracket();
+	drawLeftLine(offset);
+	drawRightLine(offset);
+	drawBottomLine(offset);
+	animate(offset + 1);
 }
 
-defineLeftBracket();
-defineRightBracket();
 animate();
