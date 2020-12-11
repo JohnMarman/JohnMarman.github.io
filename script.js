505a60f40cba444e1814dbb36b486c297da445e5
JohnM <17261899+JohnMarman@users.noreply.github.com>
2020-12-10 20:08:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function() {
           // Switch the class on the previously active div to make it hidden
           $('.active-demo').removeClass('active-demo').addClass('inactive');
           // Switch the class on the new active div to show it
-          var selectorForActiveDemo = $(this).attr('href');
+          var selectorForActiveDemo = $(this);
           $(selectorForActiveDemo).removeClass('inactive').addClass('active-demo');
      });
 });
