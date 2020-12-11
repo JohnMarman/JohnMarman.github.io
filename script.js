c83a287babe7aae1359b04674597992e49788928
JohnM <17261899+JohnMarman@users.noreply.github.com>
2020-12-10 20:09:44 -0500

    idk

--- script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-     $('.tab-link').on('click', function() {
-          // Switch the class on the previously active div to make it hidden
-          $('.active-demo').removeClass('active-demo').addClass('inactive');
-          // Switch the class on the new active div to show it
-          var selectorForActiveDemo = $(this);
-          $(selectorForActiveDemo).removeClass('inactive').addClass('active-demo');
-     });
-});
