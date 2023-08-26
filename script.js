5159e3415ea0b4972850c318285752efc493b4eb
JohnMarman <17261899+JohnMarman@users.noreply.github.com>
2023-08-26 17:04:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ let timer;
 
 function showExpanded(tileId) {
   const tile = document.getElementById(tileId);
+  tile.classList.add("expanded");
   tile.style.position = "fixed";
   tile.style.top = "0";
   tile.style.left = "0";
   tile.style.width = "100%";
   tile.style.height = "100vh";
   tile.style.zIndex = "1000";
+  tile.style.margin = "0"; // Remove extra margins
+  tile.style.padding = "0"; // Remove extra padding
   
   document.getElementById("back-button").style.display = "block";
 }
@@ -15,12 +18,15 @@ function showExpanded(tileId) {
 function hideExpanded() {
   const tiles = document.querySelectorAll(".tile");
   tiles.forEach(tile => {
+    tile.classList.remove("expanded");
     tile.style.position = "relative";
     tile.style.top = "initial";
     tile.style.left = "initial";
     tile.style.width = "200px";
     tile.style.height = "200px";
     tile.style.zIndex = "initial";
+    tile.style.margin = "20px"; // Reset to original margin
+    tile.style.padding = "initial"; // Reset to original padding
   });
   
   document.getElementById("back-button").style.display = "none";
