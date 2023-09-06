37658a923c11af1c1f5943ad0f3571a12d9cd2da
JohnMarman <17261899+JohnMarman@users.noreply.github.com>
2023-09-05 22:40:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 let timer;
 
+async function getWeatherData() {
+  try {
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=42.9834&longitude=-81.233&current_weather=true');
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    const weatherData = await response.json();
+    const weatherInfoDiv = document.querySelector('.typewriter h1');
+    const temperature = weatherData.current_weather.temperature;
+    const weatherCode = weatherData.current_weather.weathercode;
+    let weatherCondition = getWeatherDescription(weatherCode);
+    
+    weatherInfoDiv.textContent = `"Good morning sir. The current weather is ${weatherCondition}, ${temperature} degrees."`;
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+function getWeatherDescription(weatherCode) {
+  if (weatherCode >= 0 && weatherCode <= 19) {
+    if (weatherCode <= 2) {
+      return 'clear';
+    } else if (weatherCode <= 6) {
+      return 'cloudy';
+    } else if ([7, 8, 9, 10, 11, 12, 18, 19, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49].includes(weatherCode)) {
+      return 'foggy';
+    } else {
+      return 'raining';
+    }
+  } else if (weatherCode >= 20 && weatherCode <= 99) {
+    if ([20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99].includes(weatherCode)) {
+      return 'raining';
+    } else if (weatherCode >= 30 && weatherCode <= 39) {
+      return 'duststorm';
+    } else if (weatherCode >= 70 && weatherCode <= 79) {
+      return 'snowing';
+    } else {
+      return 'unknown';
+    }
+  } else {
+    return 'unknown';
+  }
+}
+
 function showExpanded(tileId) {
   const tile = document.getElementById(tileId);
   tile.classList.add("expanded");
